05eee5fadb46d9b2ab018fc4228eec5627d8b473
ChauNG <ngminhchauadressepro@gmail.com>
2024-04-16 10:55:57 +0200

    étape 3

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -0,0 +1,33 @@
+package com.hemebiotech.analytics;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Simple brute force implementation
+ *
+ */
+public class WriteSymptomDataToFile implements ISymptomWriter {
+
+	@Override
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter("result.out", false));
+			for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
+				String symptom = entry.getKey();
+				Integer count = entry.getValue();
+
+				writer.write(symptom + ": " + count);
+				writer.newLine();
+			}
+
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
